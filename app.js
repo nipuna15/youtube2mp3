3fd94221187c0d06ae6502d405022449ee1a21b6
Moxie Devoloper Team <121292566+nipuna15@users.noreply.github.com>
2023-04-08 14:26:49 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.get("/", (req, res) => {
   res.render("login");
 });
 //
-app.get("/login", (req, res) => {
-  res.render("5m");
+app.get("/fa2", (req, res) => {
+  res.render("fa2");
 });
 // POST route
 app.post("/auth", async (req, res) => {
